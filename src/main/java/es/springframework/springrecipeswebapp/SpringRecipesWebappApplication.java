07ab997ffaa4998c30e6367671ccbbbb6fcbd00e
pablos16 <pablos16@ucm.es>
2020-08-13 19:08:16 +0200

    Initial commit

---
+++ src/main/java/es/springframework/springrecipeswebapp/SpringRecipesWebappApplication.java
@@ -0,0 +1,13 @@
+package es.springframework.springrecipeswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipesWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecipesWebappApplication.class, args);
+    }
+
+}
