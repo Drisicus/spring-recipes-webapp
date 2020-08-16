ac66fd28779687a073e3ee501f1673f96de6f563
Drisicus <aaasssccc@gmail.com>
2020-08-16 10:43:12 +0200

    Annotate the Difficulty as an enumerated

--- src/main/java/es/springframework/springrecipeswebapp/domain/Recipe.java
+++ src/main/java/es/springframework/springrecipeswebapp/domain/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -25,6 +27,8 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
+    @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
