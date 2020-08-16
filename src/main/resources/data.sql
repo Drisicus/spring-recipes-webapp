b6ed7bbaf3d7858434e9e2eb6f39421a1085c950
Drisicus <aaasssccc@gmail.com>
2020-08-16 12:08:42 +0200

    Create initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO category (description) VALUES ('Spanish');
+INSERT INTO category (description) VALUES ('Mexican');
+
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
