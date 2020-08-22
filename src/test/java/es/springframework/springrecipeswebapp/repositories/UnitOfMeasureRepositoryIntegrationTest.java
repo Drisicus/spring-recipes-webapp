84030816c238958a7b93cf9accaa2a2fba099a9a
Drisicus <aaasssccc@gmail.com>
2020-08-22 11:34:10 +0200

    Update of UnitOfMeasureRepositoryIntegrationTest. With JUnit5 the annotaion @RunWith is not longer required.

--- src/test/java/es/springframework/springrecipeswebapp/repositories/UnitOfMeasureRepositoryIntegrationTest.java
+++ src/test/java/es/springframework/springrecipeswebapp/repositories/UnitOfMeasureRepositoryIntegrationTest.java
@@ -3,17 +3,14 @@ package es.springframework.springrecipeswebapp.repositories;
 import es.springframework.springrecipeswebapp.domain.UnitOfMeasure;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 class UnitOfMeasureRepositoryIntegrationTest {
 
