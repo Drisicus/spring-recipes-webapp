7ade4996d8658bb2e7263230a5e20585c04dc830
Drisicus <aaasssccc@gmail.com>
2020-08-25 21:31:43 +0200

    add test

---
+++ src/test/java/es/springframework/springrecipeswebapp/converters/UnitOfMeasureToUnitOfMeasureCommandTest.java
@@ -0,0 +1,45 @@
+package es.springframework.springrecipeswebapp.converters;
+
+import es.springframework.springrecipeswebapp.commands.UnitOfMeasureCommand;
+import es.springframework.springrecipeswebapp.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnitOfMeasureToUnitOfMeasureCommandTest {
+
+    public static final String DESCRIPTION = "description";
+    public static final Long LONG_VALUE = 1L;
+
+    UnitOfMeasureToUnitOfMeasureCommand converter;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        converter = new UnitOfMeasureToUnitOfMeasureCommand();
+    }
+
+    @Test
+    public void testNullObjectConvert() throws Exception {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    public void testEmptyObj() throws Exception {
+        assertNotNull(converter.convert(new UnitOfMeasure()));
+    }
+
+    @Test
+    public void convert() throws Exception {
+        //given
+        UnitOfMeasure uom = new UnitOfMeasure();
+        uom.setId(LONG_VALUE);
+        uom.setDescription(DESCRIPTION);
+        //when
+        UnitOfMeasureCommand uomc = converter.convert(uom);
+
+        //then
+        assertEquals(LONG_VALUE, uomc.getId());
+        assertEquals(DESCRIPTION, uomc.getDescription());
+    }
+}
